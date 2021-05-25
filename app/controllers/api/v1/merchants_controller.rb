c194119526472b5034eaa169310c72a756aa3904
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-25 18:40:08 -0400

    Make postman part 1 happy

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,16 +1,17 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    if params[:per_page]
+
       page = params[:page].to_i
       per_page = params[:per_page].to_i
       render json: MerchantSerializer.new(Merchant.paginate(page, per_page))
-    else
-      render json: MerchantSerializer.new(Merchant.paginate)
-    end 
+    # else
+      # render json: MerchantSerializer.new(Merchant.paginate)
+    # end
   end
 
   def show
-    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    @merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(@merchant)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,3 @@
-class Merchant < ApplicationRecord
+class Merchant < MerchantAndItem
   has_many :items, dependent: :destroy
-
-  def self.paginate(page = 1, per_page = 20)
-    page = (page || 1).to_i if page < 1
-    limit(per_page).offset((page - 1) * per_page)
-  end
 end

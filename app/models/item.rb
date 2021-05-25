176092f600239640bb9209816117176408a5c7f4
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-25 16:38:50 -0400

    Recreate migrations and pass tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
 
-  def self.paginate(page, per_page = 20)
-    page = 1 if page < 1
+  def self.paginate(page = 1, per_page = 20)
+    page = (page || 1).to_i if page < 1
     limit(per_page).offset((page - 1) * per_page)
   end
 end

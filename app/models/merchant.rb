461a5890d3e70b5ff4049a914a806df62b9eb1d9
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-25 17:25:04 -0400

    Implement pagination for merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
+
+  def self.paginate(page = 1, per_page = 20)
+    page = (page || 1).to_i if page < 1
+    limit(per_page).offset((page - 1) * per_page)
+  end
 end

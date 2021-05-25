ff8b98e0d2a1d42b20c4b6ba5651750f780bb77c
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-25 17:24:50 -0400

    Add tests for merchants pagination

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it {should have_many :items}
   end
+
+  describe 'class methods' do
+    describe '::paginate' do
+      it 'displays a variable amount of merchants per page' do
+        create_list(:merchant, 55)
+
+        expect(Merchant.paginate(1, 20).count).to eq(20)
+        expect(Merchant.paginate(2, 20).count).to eq(20)
+        expect(Merchant.paginate(3, 20).count).to eq(15)
+      end
+    end
+  end
 end

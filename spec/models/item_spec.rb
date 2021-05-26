f05281831a7c40a6f8309e43c4258f138e9af63f
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-26 18:15:54 -0400

    Add test for all items matching term and min price

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,5 +19,23 @@ RSpec.describe Item, type: :model do
         expect(Item.paginate(2, 20).count).to eq(15)
       end
     end
+
+    describe '::name_includes' do
+      it 'returns all items with name that matches search term' do
+        merchant = create(:merchant).id
+
+        bottle = create(:item, merchant_id: merchant, name: "Black Hydroflask Water Bottle", unit_price: 35.22)
+        lamp = create(:item, merchant_id: merchant, name: "Dimmable Desk Lamp", unit_price: 50.10)
+        marker = create(:item, merchant_id: merchant, name: "Felt Tip Marker - Black", unit_price: 5.78)
+        headphones = create(:item, merchant_id: merchant, name: "Wireless Black Headphones", unit_price: 149.99)
+        frame = create(:item, merchant_id: merchant, name: "Allblack Matte Frame", unit_price: 1.99)
+
+        matching_items = Item.name_includes("black")
+
+        expect(matching_items.count).to eq(4)
+        expect(matching_items.include?(lamp)).to eq(false)
+        expect(matching_items.include?(headphones)).to eq(true)
+      end
+    end
   end
 end

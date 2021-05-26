cd2e5ab08a987a56833495d4e71b7fe4e90c3a6e
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-25 21:04:09 -0400

    Remove commented code

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,13 +1,9 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-
-      page = params[:page].to_i
-      per_page = params[:per_page].to_i
-      render json: MerchantSerializer.new(Merchant.paginate(page, per_page))
-    # else
-      # render json: MerchantSerializer.new(Merchant.paginate)
-    # end
+    page = params[:page].to_i
+    per_page = params[:per_page].to_i
+    render json: MerchantSerializer.new(Merchant.paginate(page, per_page))
   end
 
   def show

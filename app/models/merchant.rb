0cec4a41e57fa11d4289eb76e784f5cc602ed731
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-27 18:25:02 -0400

    Create query for merchant total revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,18 @@
 class Merchant < MerchantAndItem
   has_many :items, dependent: :destroy
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   def self.name_includes(term)
     where("name ilike ?", "%#{term}%").order(name: :asc).first
+  end
+
+  def total_revenue
+    transactions
+    .where("invoices.status = 'shipped'")
+    .where("transactions.result = 'success'")
+    .pluck("(invoice_items.quantity * items.unit_price) AS revenue")
+    .sum
   end
 end

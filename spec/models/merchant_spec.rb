84b74de63bf61a05e42037ae1ff8e8b9ce66d224
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-26 21:52:50 -0400

    Add test for merchant search by name fragment

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -15,5 +15,18 @@ RSpec.describe Merchant, type: :model do
         expect(Merchant.paginate(3, 20).count).to eq(15)
       end
     end
+
+    describe '::name_includes' do
+      it 'returns the first alphabetically matched merchant with search term' do
+        tee = create(:merchant, name: "Cotton Apparel")
+        toy = create(:merchant, name: "Cat Cottage House")
+        pen = create(:merchant, name: "Apricot Fountain Pens")
+        pie = create(:merchant, name: "Vegan Pies")
+
+        matched_merchant = Merchant.name_includes("cot")
+
+        expect(matched_merchant.name).to eq(pen.name)
+      end
+    end
   end
 end

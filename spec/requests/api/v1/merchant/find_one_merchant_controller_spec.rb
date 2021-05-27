78d170c6dea4c1b91b11292f76f88342392c6ffa
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-26 21:52:11 -0400

    Add test to find one merchant with search term

---
+++ spec/requests/api/v1/merchant/find_one_merchant_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Find Merchant' do
+  describe 'Returns one merchant' do
+    it 'That matches a search term' do
+      tee = create(:merchant, name: "Cotton Apparel")
+      toy = create(:merchant, name: "Cat Cottage House")
+      pen = create(:merchant, name: "Apricot Fountain Pens")
+      pie = create(:merchant, name: "Vegan Pies")
+
+      term = "cot"
+      get "/api/v1/merchants/find?name=#{term}"
+
+      expect(response).to be_successful
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to eq("#{pen.id}")
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to eq(pen.name)
+
+      expect(merchant.count).to eq(1)
+    end
+  end
+end
